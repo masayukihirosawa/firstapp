9f894345e85b5e372f83c2175c2fecaef55867e6
EC2 Default User <ec2-user@ip-172-31-33-119.ap-northeast-1.compute.internal>
2020-12-13 19:20:10 +0900

    kyouyuu access seigen

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,5 +1,6 @@
 class LikesController < ApplicationController
   before_action :authenticate_user
+  before_action :ensure_correct_user, {only: [:create, :destroy]}
   
   def create
     @like = Like.new(user_id: @current_user.id, post_id: params[:post_id])
@@ -11,6 +12,14 @@ class LikesController < ApplicationController
     @like = Like.find_by(user_id: @current_user.id, post_id: params[:post_id])
     @like.destroy
     redirect_to("/posts/index")
+  end
+  
+  def ensure_correct_user
+    @post = Post.find_by(id: params[:post_id])
+    if @current_user.id != @post.user_id
+      flash[:notice] = "権限がありません"
+      redirect_to("/posts/index")
+    end
   end
   
 end
