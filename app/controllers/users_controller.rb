7b0a0f8c97c111949f2581004398193d24d4b716
EC2 Default User <ec2-user@ip-172-31-33-119.ap-northeast-1.compute.internal>
2020-12-14 19:07:22 +0900

    user_show access_seigen

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   
   before_action :authenticate_user, {only: [:index, :show, :edit, :update]}
   before_action :forbid_login_user, {only: [:new, :create, :login, :login_form]}
-  before_action :ensure_correct_user, {only: [:edit, :update]}
+  before_action :ensure_correct_user, {only: [:show, :edit, :update]}
   
   def index
     @users = User.all
